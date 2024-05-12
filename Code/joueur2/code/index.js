3652e8713fcc7630e260d177cf3cf39bab1d0c87
Malick <151407711+ABDULMVLICK@users.noreply.github.com>
2024-05-12 22:05:39 +0200

    Add files via upload

---
+++ Code/joueur2/code/index.js
@@ -0,0 +1,13 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.slider2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
---
+++ acceuil.js
@@ -0,0 +1,28 @@
+$('.slider').slick({
+    centerMode: true,
+    autoplay: true,
+    speed: 300,
+    autoplaySpeed: 2000,
+    centerPadding: '50px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
